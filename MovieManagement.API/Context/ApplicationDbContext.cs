3a214111d04781d4a9b5b33805589698c67943c2
majastarovlas123 <starovlasmaja@gmail.com>
2022-12-08 15:48:09 +0100

    Create ApplicationDbContext.

---
+++ MovieManagement.API/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.API.DbModels;
+
+namespace MovieManagement.API.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieType> MovieTypes { get; set; }
+    }
+}
